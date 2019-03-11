8536f358d5658d8b22f68eb6b1fa47dd13686b95
Ashutosh Kumar <g.oneashu@gmail.com>
2019-03-12 02:47:37 +0530

    pagination added to limit the content

--- blog_site/blog/urls.py
+++ blog_site/blog/urls.py
@@ -4,13 +4,15 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
-    PostDeleteView
+    PostDeleteView,
+    UserPostListView
 )
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user_posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
