d414a5b231c92d5b16ad7199c6197d86f5e8843f
Ashutosh Kumar <g.oneashu@gmail.com>
2019-03-12 21:00:50 +0530

    Forget password field added on the login page

--- blog_site/django_project/settings.py
+++ blog_site/django_project/settings.py
@@ -130,3 +130,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+#Your email address will go here
+EMAIL_HOST_USER = ''
+#Related password will go here
+EMAIL_HOST_PASSWORD = ''
